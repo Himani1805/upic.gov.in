0186700d8d09d4c7d03ff39f25cb76c9b0199d19
0009629 <0009629@su.edu.in>
2025-01-21 00:18:06 +0530

    Activity has updated

---
+++ activity.js
@@ -0,0 +1,31 @@
+const activity = [
+    {
+        title: "परिपत्र पत्रांक 196 /नि०स० /रा ० मु ० सू ० आ ० / 2022 दिनांकित 10 जून , 2022   [ Notice Board ]",
+        link: "https://upic.gov.in/MediaGallery/Circular%20No%20196.pdf",
+        icon: "https://cdn-icons-png.flaticon.com/128/5968/5968377.png",
+        size:"Size: 2 MB",
+        date:"Date : 17/06/2022",  
+    },
+    {
+        title: "कार्यालय आदेश दिनांक 18/02/2022 (पत्रांक :20/ सचिव कैम्प सू. आ. / 2022 )   [ Notice Board ]",
+        link: "https://upic.gov.in/MediaGallery/18-02-2022.pdf",
+        icon: "https://cdn-icons-png.flaticon.com/128/5968/5968377.png",
+        size:"Size: 772 KB",
+        date:"Date : 18/02/2022",   
+    }, 
+    {
+        title: "आयोग में मुख्य सतर्कता अधिकारी और सतर्कता अधिकारी का मनोनयन [ Notice Board ]",
+        link: "https://upic.gov.in/MediaGallery/satarkata_15-07-2021.pdf",
+        icon: "https://cdn-icons-png.flaticon.com/128/5968/5968377.png",
+        size:"Size: 738 KB",
+        date:"Date : 15/07/2021",   
+    }, 
+    {
+        title: "प्रतिलिपि से संबंधित कार्यालय आदेश [ Notice Board ]",
+        link: "https://upic.gov.in/MediaGallery/copy%20of%20office%20order.pdf",
+        icon: "https://cdn-icons-png.flaticon.com/128/5968/5968377.png",
+        size:"Size: 582 KB",
+        date:"Date : 14/12/2021",
+    }, 
+
+];
